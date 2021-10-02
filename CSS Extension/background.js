9e9668b61ec7df28dae7edb0d7d98b6d661666e0
Your Name <elliotvanh@gmail.com>
2021-10-02 14:10:35 -0400

    base changes and allow rigth-click addition)

--- CSS Extension/background.js
+++ CSS Extension/background.js
@@ -4,3 +4,15 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+function getClickHandler() {
+  console.log('hello');
+};
+
+chrome.contextMenus.create({
+  "title": "TEST",
+  "type": "normal",
+  "contexts": ["selection"],
+  "onclick": getClickHandler(),
+  "id": "ID"
+});
