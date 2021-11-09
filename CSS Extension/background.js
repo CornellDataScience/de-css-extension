d0bf6e914a594be2091017f892cc2d06e0cff48e
Arjun Shah <ans96@cornell.edu>
2021-11-09 17:56:15 -0500

    simplified logic for changing text color

--- CSS Extension/background.js
+++ CSS Extension/background.js
@@ -60,121 +60,20 @@ function getSiblings (elem) {
 }
 
 function changeTextColor() {
-  // get container element of selected text 
-  var text = "", containerElement = null;
-  if (typeof window.getSelection != "undefined") {
-    var sel = window.getSelection();
-    if (sel.rangeCount) {
-      var node = sel.getRangeAt(0).commonAncestorContainer;
-      containerElement = node.nodeType == 1 ? node : node.parentNode;
-      text = sel.toString();
-    }
-  } else if (typeof document.selection != "undefined" &&
-    document.selection.type != "Control") {
-    var textRange = document.selection.createRange();
-    containerElement = textRange.parentElement();
-    text = textRange.text;
+  sel = window.getSelection();
+  if (sel.rangeCount && sel.getRangeAt) {
+    range = sel.getRangeAt(0);
   }
-
-  var originalContainer = containerElement;
-
-    // calculating offset for first parent with id
-    let offset = 0;
-    while (containerElement.id == "") {
-      let containerHtmlString = containerElement.outerHTML;
-
-      offset += containerHtmlString.indexOf(">") - containerHtmlString.indexOf("<") + 1
-
-      // find out position of current child in list
-      siblings_exclusive = getSiblings(containerElement);
-      siblings_inclusive = containerElement.parentElement.children;
-
-      if (siblings_exclusive.length != 0) {
-        index = siblings_exclusive.length-1;
-        for (let i=0;i<siblings_exclusive.length;i++) {
-          if (siblings_exclusive[i].innerHTML != siblings_inclusive[i].innerHTML) {
-            index = i - 1;
-            break;
-          }
-        }
-
-        for (let i=0;i<=index;i++) {
-          offset += siblings_exclusive[i].outerHTML.length;
-        }
-      } 
-
-      containerElement = containerElement.parentElement;
-    }
-
-    id = containerElement.id;
-
-    var selection = window.getSelection();
-    var start = selection.anchorOffset;
-    var end = selection.focusOffset;
-
-    offset += originalContainer.innerHTML.indexOf(selection.anchorNode.wholeText);
-
-    var str = $('#'+id).html();
-    str = str.replaceAll('\n','');
-
-    uncomment = "";
-    for(let i=0;i<str.length-4;i++) {
-      if (str.substring(i,i+4) == "<!--") {
-        for (let j=i+4; j<str.length-3; j++) {
-          if (str.substring(j,j+3) == "-->") {
-            uncomment = str.substring(0,i) + str.substring(j+3);
-            break;
-          }
-        }
-      }
-    }
-
-    if (uncomment != "") {
-      str = uncomment;
-    }
-
-    // adding offset to start and end indices of selection
-    var startInd = [start + offset];
-    var lastInd = [end + offset];
-
-    let pre = str.substring(0, startInd[0]);
-    let post = str.substring(lastInd[0], str.length);
-    let phrase = str.substring(startInd[0], lastInd[0]);
-
-  str = pre + `<span style="color:red">${phrase}</span>` + post;
-
-  /*
-  var txt = this.innerText;
-  var selection = window.getSelection();
-  var start = selection.anchorOffset;
-  var end = selection.focusOffset;
-
-  var startInd = [start];
-  var lastInd = [end];
-  var count = startInd.length;
-
-  
-  let pre = str.substring(0, startInd[i]);
-  let post = str.substring(lastInd[i], str.length);
-  let phrase = str.substring(startInd[i], lastInd[i]);
-
-  let nextPhrase;
-
-  if (i < count - 1) {
-    nextPhrase = str.substring(startInd[i + 1], lastInd[i + 1]);
+  // Set design mode to on
+  document.designMode = "on";
+  if (range) {
+    sel.removeAllRanges();
+    sel.addRange(range);
   }
-
-str = pre + `<div style='display:inline; color:#ed3833; cursor:pointer;'>${phrase}</div>` + post;
-
-if (i < count - 1) {
-  startInd[i + 1] = str.indexOf(nextPhrase, startInd[i + 1]) - 1;
-  lastInd[i + 1] = startInd[i + 1] + nextPhrase.length;
-}
-
-$(containerElement.id).html(str);
-*/
-
-  $('#'+id).html(str);
+  // Colorize text
+  document.execCommand("ForeColor", false, "red");
+  // Set design mode to off
+  document.designMode = "off";
 
 }
 
